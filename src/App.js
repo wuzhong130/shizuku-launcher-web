123a99620c3a6a86b37973446bac2d823a896913
Seraphim Lou <loujunhuimain@icloud.com>
2022-08-20 16:50:30 +0800

    Add State Feature && Add Change Instance IP Feature && Add Terminate Instance Feature && App Improvements

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { styled } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import Select from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
@@ -75,6 +76,7 @@ function App() {
   const systems = ["Debian 10", "Debian 11", "Ubuntu 20.04", "Ubuntu 22.04", "Arch Linux"]
   const types = ["t2.micro", "t3.micro", "c5n.large", "t3a.micro", "t2.2xlarge", "t2.xlarge", "t2.large", "t2.medium", "t2.nano", "t3.nano", "t3.small", "t3.medium", "t3.large", "t3.xlarge", "t3.2xlarge", "t3a.nano", "t3a.small", "t3a.medium", "t3a.large", "t3a.xlarge", "t3a.2xlarge", "c5n.xlarge", "c5n.4xlarge", "c5n.2xlarge", "c5.xlarge", "c5.2xlarge", "c5.4xlarge", "c5a.large", "c5a.xlarge", "c5a.2xlarge"]
   const regionsDetail = ["US East (Ohio)", "US East (N. Virginia)", "US West (N. California)", "US West (Oregon)", "Africa (Cape Town)", "Asia Pacific (Hong Kong)", "Asia Pacific (Jakarta)", "Asia Pacific (Mumbai)", "Asia Pacific (Osaka)", "Asia Pacific (Seoul)", "Asia Pacific (Singapore)", "Asia Pacific (Sydney)", "Asia Pacific (Tokyo)", "Canada (Central)", "Europe (Frankfurt)", "Europe (Ireland)", "Europe (London)", "Europe (Milan)", "Europe (Paris)", "Europe (Stockholm)", "Middle East (Bahrain)", "South America (São Paulo)"]
+  const states = new Map([[0, "正在启动"], [16, "正在运行"], [32, "正在关机"], [48, "已终止"], [64, "正在停止"], [80, "已停止"]]);
 
   const [aki, setAki] = useState("");
   const [saki, setSaki] = useState("");
@@ -94,6 +96,9 @@ function App() {
   const [isGettingQuota, setIsGettingQuota] = useState(false);
   const [isCheckingInstances, setIsCheckingInstances] = useState(false);
   const [isCheckedInstances, setIsCheckedInstances] = useState(false);
+  const [regionOfCheckedInstances, setRegionOfCheckedInstances] = useState("");
+  const [idOfInstanceChangingIp, setIdOfInstanceChangingIp] = useState("");
+  const [idOfInstanceTerminating, setIdOfInstanceTerminating] = useState("");
   const [instances, setInstances] = useState([]);
 
   //Interactions
@@ -310,6 +315,7 @@ function App() {
                           showAlert("启动实例成功", "您的新实例id为" + data.Instances[0].InstanceId + "，请30秒后通过查询实例详细信息获得公网ip，在此期间您可以开启另一个实例或进行其他操作");
                           setIsLaunchingInstance(false);
                           setInstances([]);
+                          checkInstances(true);
                         }
                       });
                     }
@@ -359,6 +365,7 @@ function App() {
             showAlert("启动实例成功", "您的新实例id为" + body.instanceId + "，请30秒后通过查询实例详细信息获得公网ip，在此期间您可以开启另一个实例或进行其他操作");
             setIsLaunchingInstance(false);
             setInstances([]);
+            checkInstances(true);
           }
           else {
             showAlert("启动实例失败：" + body.error.name, "错误：" + body.error.message + "，请再试一次或联系支持");
@@ -460,9 +467,10 @@ function App() {
     }
   }
 
-  function checkInstances() {
+  function checkInstances(noSuccessAlert) {
     setIsCheckingInstances(true);
     setIsCheckedInstances(false);
+    setRegionOfCheckedInstances("");
     if (aki.length !== 20 || saki.length !== 40) {
       showAlert("无效凭证", "请检查凭证格式是否正确");
       setIsCheckingInstances(false);
@@ -508,13 +516,16 @@ function App() {
           var processedInstances = []
           data.Reservations.forEach(reservation => {
             reservation.Instances.forEach(instance => {
-              processedInstances.push({ id: instance.InstanceId, type: instance.InstanceType, ip: instance.PublicIpAddress, platform: instance.PlatformDetails })
+              processedInstances.push({ id: instance.InstanceId, state: instance.State.Code, type: instance.InstanceType, ip: instance.PublicIpAddress, platform: instance.PlatformDetails })
             })
           })
           setInstances(processedInstances);
-          showAlert("查看实例详细信息成功", "请在查看实例详细信息选项卡中查看您在该区域的实例信息");
+          if (!noSuccessAlert) {
+            showAlert("查看实例详细信息成功", "请在查看实例详细信息选项卡中查看您在该区域的实例信息");
+          }
           setIsCheckingInstances(false);
           setIsCheckedInstances(true);
+          setRegionOfCheckedInstances(ciRegion);
         }
       });
     }
@@ -548,13 +559,233 @@ function App() {
           var body = await response.json();
           if (response.ok) {
             setInstances(body.instances);
-            showAlert("查看实例详细信息成功", "请在查看实例详细信息选项卡中查看您在该区域的实例信息");
+            if (!noSuccessAlert) {
+              showAlert("查看实例详细信息成功", "请在查看实例详细信息选项卡中查看您在该区域的实例信息");
+            }
             setIsCheckingInstances(false);
             setIsCheckedInstances(true);
+            setRegionOfCheckedInstances(ciRegion);
           }
           else {
             showAlert("查看实例详细信息失败：" + body.error.name, "错误：" + body.error.message + "，请再试一次或联系支持");
             setIsCheckingInstances(false);
+          }
+        });
+    }
+  }
+
+  function changeInstanceIp(id) {
+    setIdOfInstanceChangingIp(id);
+    if (aki.length !== 20 || saki.length !== 40) {
+      showAlert("无效凭证", "请检查凭证格式是否正确");
+      setIdOfInstanceChangingIp("");
+      return;
+    }
+    if ((mode === 2 || mode === 3 || mode === 4) && !validateRemote()) {
+      showAlert("无效远端地址", "远端地址格式不正确，请修改后再试一次");
+      setIdOfInstanceChangingIp("");
+      return;
+    }
+    if ((mode === 3 || mode === 4) && !validateProxy()) {
+      showAlert("无效代理地址", "代理地址格式不正确，请修改后再试一次");
+      setIdOfInstanceChangingIp("");
+      return;
+    }
+    if (mode === 1 || mode === 3) {
+      AWS.config = new AWS.Config();
+      AWS.config.update(
+        {
+          accessKeyId: aki,
+          secretAccessKey: saki,
+          region: regionOfCheckedInstances
+        }
+      );
+      if (mode === 3) {
+        AWS.config.update({
+          httpOptions: { agent: ProxyAgent(proxy) }
+        });
+      }
+      var ec2 = new AWS.EC2();
+      var allocateParams = {
+        Domain: "vpc"
+      };
+      ec2.allocateAddress(allocateParams, function (err, data) {
+        if (err) {
+          showAlert("更换实例ip失败：" + err.name, "错误：" + err.message + " 请再试一次或联系支持");
+          setIdOfInstanceChangingIp("");
+        }
+        else {
+          var newAllocationId = data.AllocationId;
+          var associateParams = {
+            AllocationId: newAllocationId,
+            InstanceId: id,
+          };
+          ec2.associateAddress(associateParams, function (err, data) {
+            if (err) {
+              showAlert("更换实例ip失败：" + err.name, "错误：" + err.message + " 请再试一次或联系支持");
+              setIdOfInstanceChangingIp("");
+            }
+            else {
+              var disassociateParams = {
+                AssociationId: data.AssociationId
+              };
+              ec2.disassociateAddress(disassociateParams, function (err, data) {
+                if (err) {
+                  showAlert("更换实例ip失败：" + err.name, "错误：" + err.message + " 请再试一次或联系支持");
+                  setIdOfInstanceChangingIp("");
+                }
+                else {
+                  var releaseParams = {
+                    AllocationId: newAllocationId
+                  };
+                  ec2.releaseAddress(releaseParams, function (err, data) {
+                    if (err) {
+                      showAlert("更换实例ip失败：" + err.name, "错误：" + err.message + " 请再试一次或联系支持");
+                      setIdOfInstanceChangingIp("");
+                    }
+                    else {
+                      setIdOfInstanceChangingIp("");
+                      showAlert("更换实例ip成功", "请等待实例详细信息重新加载");
+                      checkInstances(true);
+                    }
+                  });
+                }
+              });
+            }
+          });
+        }
+      });
+    }
+    else if (mode === 2 || mode === 4) {
+      var ciiPostBody
+      if (mode === 2) {
+        ciiPostBody = JSON.stringify({
+          aki: aki,
+          saki: saki,
+          region: regionOfCheckedInstances,
+          instanceId: id,
+          useProxy: false
+        })
+      }
+      else if (mode === 4) {
+        ciiPostBody = JSON.stringify({
+          aki: aki,
+          saki: saki,
+          region: regionOfCheckedInstances,
+          instanceId: id,
+          useProxy: true,
+          proxy: proxy
+        })
+      }
+      fetch(remote + '/aws-change-instance-ip', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: ciiPostBody
+      })
+        .then(async (response) => {
+          var body = await response.json();
+          if (response.ok) {
+            showAlert("更换实例ip成功", "请等待实例详细信息重新加载");
+            setIdOfInstanceChangingIp("");
+            checkInstances(true);
+          }
+          else {
+            showAlert("更换实例ip失败：" + body.err.name, "错误：" + body.err.message + " 请再试一次或联系支持");
+            setIdOfInstanceChangingIp("");
+          }
+        });
+    }
+  }
+
+  function terminateInstance(id) {
+    setIdOfInstanceTerminating(id);
+    if (aki.length !== 20 || saki.length !== 40) {
+      showAlert("无效凭证", "请检查凭证格式是否正确");
+      setIdOfInstanceTerminating("");
+      return;
+    }
+    if ((mode === 2 || mode === 3 || mode === 4) && !validateRemote()) {
+      showAlert("无效远端地址", "远端地址格式不正确，请修改后再试一次");
+      setIdOfInstanceTerminating("");
+      return;
+    }
+    if ((mode === 3 || mode === 4) && !validateProxy()) {
+      showAlert("无效代理地址", "代理地址格式不正确，请修改后再试一次");
+      setIdOfInstanceTerminating("");
+      return;
+    }
+    if (mode === 1 || mode === 3) {
+      AWS.config = new AWS.Config();
+      AWS.config.update(
+        {
+          accessKeyId: aki,
+          secretAccessKey: saki,
+          region: regionOfCheckedInstances
+        }
+      );
+      if (mode === 3) {
+        AWS.config.update({
+          httpOptions: { agent: ProxyAgent(proxy) }
+        });
+      }
+      var ec2 = new AWS.EC2();
+      var params = {
+        InstanceIds: [
+          id
+        ]
+      };
+      ec2.terminateInstances(params, function (err, data) {
+        if (err) {
+          showAlert("终止实例失败：" + err.name, "错误：" + err.message + "，请再试一次或联系支持");
+          setIdOfInstanceTerminating("");
+        }
+        else {
+          showAlert("终止实例成功", "请等待实例详细信息重新加载，实例将在不久后被删除，在此之前它会保留在列表一段时间");
+          setIdOfInstanceTerminating("");
+          checkInstances(true);
+        }
+      });
+    }
+    else if (mode === 2 || mode === 4) {
+      var tiPostBody
+      if (mode === 2) {
+        tiPostBody = JSON.stringify({
+          aki: aki,
+          saki: saki,
+          region: regionOfCheckedInstances,
+          instanceId: id,
+          useProxy: false
+        })
+      }
+      else if (mode === 4) {
+        tiPostBody = JSON.stringify({
+          aki: aki,
+          saki: saki,
+          region: regionOfCheckedInstances,
+          instanceId: id,
+          useProxy: true,
+          proxy: proxy
+        })
+      }
+      fetch(remote + '/aws-terminate-instance', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: tiPostBody
+      })
+        .then(async (response) => {
+          var body = await response.json();
+          if (response.ok) {
+            showAlert("终止实例成功", "实例将在不久后被删除，在此之前它会保留在列表一段时间");
+            setIdOfInstanceTerminating("");
+            checkInstances(true);
+          }
+          else {
+            showAlert("终止实例失败：" + body.err.name, "错误：" + body.err.message + "，请再试一次或联系支持");
+            setIdOfInstanceTerminating("");
           }
         });
     }
@@ -785,18 +1016,31 @@ function App() {
                 <TableHead>
                   <TableRow>
                     <TableCell>id</TableCell>
+                    <TableCell>状态</TableCell>
                     <TableCell>公网ip</TableCell>
                     <TableCell>实例类型</TableCell>
                     <TableCell>操作系统</TableCell>
+                    <TableCell>操作</TableCell>
                   </TableRow>
                 </TableHead>
                 <TableBody>
                   {instances.map((row) => (
                     <TableRow key={row.id}>
                       <TableCell>{row.id}</TableCell>
+                      <TableCell>{states.get(row.state)}</TableCell>
                       <TableCell>{row.ip}</TableCell>
                       <TableCell>{row.type}</TableCell>
                       <TableCell>{row.platform}</TableCell>
+                      <TableCell>
+                        <Box sx={{ '& button': { m: 1 } }}>
+                          {idOfInstanceChangingIp === row.id || idOfInstanceTerminating === row.id ? (<CircularProgress />) : (
+                            <div>
+                              <Button size="small" variant="outlined" onClick={() => changeInstanceIp(row.id)}>更换ip</Button>
+                              <Button size="small" variant="outlined" color="error" onClick={() => terminateInstance(row.id)}>终止</Button>
+                            </div>
+                          )}
+                        </Box>
+                      </TableCell>
                     </TableRow>
                   ))}
                 </TableBody>
